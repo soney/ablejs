2d3bfac9a436427ec3a972d35e45c86826936b56
Stephen Oney <swloney@gmail.com>
2013-04-09 12:56:42 -0700

    jsl fixes

--- able.js
+++ able.js
@@ -1,8 +1,142 @@
-/*jslint nomen: true  vars: true */
+/*jslint nomen: true  vars: true, eqeq: true */
 /*global window */
 
 var able = (function (root) {
     "use strict";
+    
+    //
+    // ============== UTILITY FUNCTIONS ============== 
+    //
+    var ArrayProto = Array.prototype, ObjProto = Object.prototype, FuncProto = Function.prototype;
+    var slice = ArrayProto.slice,
+        toString = ObjProto.toString,
+        nativeForEach = ArrayProto.forEach,
+        nativeMap = ArrayProto.map;
+    
+    // Is a given value an array?
+    // Delegates to ECMA5's native Array.isArray
+    var isArray = Array.isArray || function (obj) {
+        return toString.call(obj) === '[object Array]';
+    };
+
+      
+
+    // Is a given value a function?
+    var isFunction = function (obj) {
+        return toString.call(obj) === '[object Function]';
+    };
+    
+    // Is a given variable a string?
+    var isString = function (obj) {
+        return toString.call(obj) === '[object String]';
+    };
+    
+    // Is a given variable an arguments object?
+    var isArguments = function (obj) {
+        return toString.call(obj) === '[object Arguments]';
+    };
+    
+    // Establish the object that gets returned to break out of a loop iteration.
+    var breaker = {};
+    
+    var extend = function (obj) {
+        var args = slice.call(arguments, 1),
+            i,
+            len = args.length,
+            prop;
+        for (i = 0; i < len; i += 1) {
+            var source = args[i];
+            for (prop in source) {
+                if (source.hasOwnProperty(prop)) {
+                    obj[prop] = source[prop];
+                }
+            }
+        }
+        return obj;
+    };
+    
+    var last = function (arr) {
+        return arr[arr.length - 1];
+    };
+    
+    var each = function (obj, iterator, context) {
+        var i, l, key;
+        if (!obj) { return; }
+        if (nativeForEach && obj.forEach === nativeForEach) {
+            obj.forEach(iterator, context);
+        } else if (obj.length === +obj.length) {
+            l = obj.length;
+            for (i = 0; i < l; i += 1) {
+                if (obj.hasOwnProperty(i) && iterator.call(context, obj[i], i, obj) === breaker) { return; }
+            }
+        } else {
+            for (key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    if (iterator.call(context, obj[key], key, obj) === breaker) { return; }
+                }
+            }
+        }
+    };
+    
+    var map = function (obj, iterator, context) {
+        var results = [];
+        if (!obj) { return results; }
+        if (nativeMap && obj.map === nativeMap) { return obj.map(iterator, context); }
+        each(obj, function (value, index, list) {
+            results[results.length] = iterator.call(context, value, index, list);
+        });
+        if (obj.length === +obj.length) { results.length = obj.length; }
+        return results;
+    };
+    
+    var nativeFilter = Array.prototype.filter;
+    var filter = function (obj, iterator, context) {
+        var results = [];
+        if (!obj) { return results; }
+        if (nativeFilter && obj.filter === nativeFilter) { return obj.filter(iterator, context); }
+        var i, len = obj.length, value;
+        for (i = 0; i < len; i += 1) {
+            value = obj[i];
+            if (iterator.call(context, value, i, obj)) { results.push(value); }
+        }
+        return results;
+    };
+      
+    // Returns everything but the first entry of the array. Aliased as `tail`.
+    // Especially useful on the arguments object. Passing an **index** will return
+    // the rest of the values in the array from that index onward. The **guard**
+    // check allows it to work with `_.map`.
+    var rest = function (array, index, guard) {
+        return slice.call(array, (index == null) || guard ? 1 : index);
+    };
+    
+    // Keep the identity function around for default iterators.
+    var identity = function (value) {
+        return value;
+    };
+    
+    // Retrieve the values of an object's properties.
+    var values = function (obj) {
+        return map(obj, identity);
+    };
+    
+    // Safely convert anything iterable into a real, live array.
+    var toArray = function (obj) {
+        if (!obj) { return []; }
+        if (isArray(obj)) { return slice.call(obj); }
+        if (isArguments(obj)) { return slice.call(obj); }
+        if (obj.toArray && isFunction(obj.toArray)) { return obj.toArray(); }
+        return values(obj);
+    };
+    
+    
+    var hasOwnProperty = Object.prototype.hasOwnProperty;
+    var has = function (obj, key) {
+        return hasOwnProperty.call(obj, key);
+    };
+    
+
+    
     var old_able = root.able;
     var able = function () { };
     able.version = "0.1.0";
@@ -91,30 +225,13 @@ var able = (function (root) {
             delete instance.forward;
         };
     }());
-        
-    
-    var extend = function (obj) {
-        var i, prop;
-        for (i = 1; i < arguments.length; i += 1) {
-            var source = arguments[i];
-            if (source) {
-                for (prop in source) {
-                    if (source.hasOwnProperty(prop)) {
-                        obj[prop] = source[prop];
-                    }
-                }
-            }
-        }
-        return obj;
-    };
-        
     
     (function () {
         var options_prop_name = "__options",
             emit_fn_name = "_emit";
         
         able.make_this_optionable = function (instance) {
-            instance[options_prop_name] = extend.apply(_, [{}].concat(Array.prototype.slice.call(arguments, 1)));
+            instance[options_prop_name] = extend.apply(this, [{}].concat(Array.prototype.slice.call(arguments, 1)));
         };
         
         able.make_proto_optionable = function (proto) {
@@ -126,7 +243,7 @@ var able = (function (root) {
                     return value;
                 }
             };
-            proto._set_option = function(key, value) {
+            proto._set_option = function (key, value) {
                 this[options_prop_name][key] = value;
             };
         
@@ -163,124 +280,6 @@ var able = (function (root) {
             };
         };
     }());
-    
-    //
-    // ============== UTILITY FUNCTIONS ============== 
-    //
-    var ArrayProto = Array.prototype, ObjProto = Object.prototype, FuncProto = Function.prototype;
-    var slice = ArrayProto.slice,
-        toString = ObjProto.toString,
-        nativeForEach = ArrayProto.forEach,
-        nativeMap = ArrayProto.map;
-    
-    // Is a given value an array?
-    // Delegates to ECMA5's native Array.isArray
-    var isArray = Array.isArray || function (obj) {
-        return toString.call(obj) === '[object Array]';
-    };
-    
-    // Retrieve the values of an object's properties.
-    var values = function (obj) {
-        return map(obj, identity);
-    };
-      
-    // Safely convert anything iterable into a real, live array.
-    var toArray = function (obj) {
-        if (!obj) { return []; }
-        if (isArray(obj)) { return slice.call(obj); }
-        if (isArguments(obj)) { return slice.call(obj); }
-        if (obj.toArray && isFunction(obj.toArray)) { return obj.toArray(); }
-        return values(obj);
-    };
-    
-    // Is a given value a function?
-    var isFunction = function (obj) {
-        return toString.call(obj) === '[object Function]';
-    };
-    
-    // Is a given variable a string?
-    var isString = function (obj) {
-        return toString.call(obj) === '[object String]';
-    };
-    
-    // Is a given variable an arguments object?
-    var isArguments = function (obj) {
-        return toString.call(obj) === '[object Arguments]';
-    };
-    
-    // Establish the object that gets returned to break out of a loop iteration.
-    var breaker = {};
-    
-    var extend = function (obj) {
-        var args = slice.call(arguments, 1),
-            i,
-            len = args.length;
-        for (i = 0; i < len; i += 1) {
-            var source = args[i];
-            for (var prop in source) {
-                obj[prop] = source[prop];
-            }
-        }
-        return obj;
-    };
-    
-    var last = function (arr) {
-        return arr[arr.length - 1];
-    };
-    
-    var each = function (obj, iterator, context) {
-        if (obj == null) { return; }
-        if (nativeForEach && obj.forEach === nativeForEach) {
-            obj.forEach(iterator, context);
-        } else if (obj.length === +obj.length) {
-            for (var i = 0, l = obj.length; i < l; i++) {
-                if (i in obj && iterator.call(context, obj[i], i, obj) === breaker) { return; }
-            }
-        } else {
-            for (var key in obj) {
-                if (has(obj, key)) {
-                    if (iterator.call(context, obj[key], key, obj) === breaker) { return; }
-                }
-            }
-        }
-    };
-    
-    var map = function (obj, iterator, context) {
-        var results = [];
-        if (obj == null) { return results; }
-        if (nativeMap && obj.map === nativeMap) { return obj.map(iterator, context); }
-        each(obj, function(value, index, list) {
-            results[results.length] = iterator.call(context, value, index, list);
-        });
-        if (obj.length === +obj.length) { results.length = obj.length; }
-        return results;
-    };
-    
-    var nativeFilter = Array.prototype.filter;
-    var filter = function (obj, iterator, context) {
-        var results = [];
-        if (obj == null) { return results; }
-        if (nativeFilter && obj.filter === nativeFilter) { return obj.filter(iterator, context); }
-        var i, len = obj.length, value;
-        for (i = 0; i<len; i++) {
-            value = obj[i];
-            if(iterator.call(context, value, i, obj)) { results.push(value); }
-        }
-        return results;
-    };
-      
-    // Returns everything but the first entry of the array. Aliased as `tail`.
-    // Especially useful on the arguments object. Passing an **index** will return
-    // the rest of the values in the array from that index onward. The **guard**
-    // check allows it to work with `_.map`.
-    var rest = function (array, index, guard) {
-        return slice.call(array, (index == null) || guard ? 1 : index);
-    };
-    
-    var hasOwnProperty = Object.prototype.hasOwnProperty;
-    var has = function (obj, key) {
-        return hasOwnProperty.call(obj, key);
-    };
     
     return able;
 }(this));
